65cbb18da9f23c6fbbdf14a0d71d5e2990dd397e
Seedling <david.r.graham32@gmail.com>
2024-04-29 11:23:21 -0400

    Create name-functions Move code to find names into their own functions in name-functions.php

--- index.php
+++ index.php
@@ -1,49 +1,19 @@
 <?php
 include 'functions/utility-functions.php';
+include 'functions/names-functions.php';
+
 $fileName = 'names-short-list.txt';
+$fullNames = load_full_names($fileName);
 
-$lineNumber = 0;
-
-// Load up the array
-$FH = fopen("$fileName", "r");
-$nextName = fgets($FH);
-
-while(!feof($FH)) {
-    if($lineNumber % 2 == 0) {
-        $fullNames[] = trim(substr($nextName, 0, strpos($nextName, " --")));
-    }
-
-$lineNumber++;
-$nextName = fgets($FH);
-}
+$firstNames = load_first_names($fullNames);
+$lastNames = load_last_names($fullNames);
+$validFullNames = load_valid_names($fullNames, $firstNames, $lastNames);
 
 // $findMe = ',';
 // echo $fullNames[0] . '<br>';
 // echo strpos($fullNames[0], $findMe) . '<br>';
 // echo substr($fullNames[0], 0, strpos($fullNames[0], $findMe));
 // exit();
-
-// Get all first names
-foreach($fullNames as $fullName) {
-    $startHere = strpos($fullName, ",") + 1;
-    $firstNames[] = trim(substr($fullName, $startHere));
-}
-
-// Get all last names
- foreach ($fullNames as $fullName) {
-     $stopHere = strpos($fullName, ",");
-     $lastNames[] = substr($fullName, 0, $stopHere);
- }
-
-// Get valid names
-for($i = 0; $i < sizeof($fullNames); $i++) {
-    // jam the first and last name toghether without a comma, then test for alpha-only characters
-    if(ctype_alpha($lastNames[$i].$firstNames[$i])) {
-        $validFirstNames[$i] = $firstNames[$i];
-        $validLastNames[$i] = $lastNames[$i];
-        $validFullNames[$i] = $validLastNames[$i] . ", " . $validFirstNames[$i];
-    }
-}
 
 // ~~~~~~~~~~~~ Display results ~~~~~~~~~~~~ //
 
@@ -74,11 +44,3 @@ echo '<ul style="list-style-type:none">';
     }
 
 ?>
-
-
-
-
-
-
-
-
