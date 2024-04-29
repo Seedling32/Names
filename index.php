30c8ee954df1b4cb0a88a1cfd96e92a31a773374
Seedling <david.r.graham32@gmail.com>
2024-04-29 16:15:58 -0400

    Finish Names by adding some styling to index.php.

--- index.php
+++ index.php
@@ -28,54 +28,77 @@ $firstNameOccurrences = max_occurrences($firstNames);
 // exit();
 
 // ~~~~~~~~~~~~ Display results ~~~~~~~~~~~~ //
+echo '<body style="background: linear-gradient(to bottom, #aaaaaa 0%,#333333 100%);margin: 0">';
+echo '<section style="background-color: #000; max-width: 85%; margin: 0 auto; padding: 2rem 0">';
+echo '<h1 style="color: #fff; font-size: 4rem; text-align:center; text-decoration: underline">Names - Results</h1>';
 
-echo '<h1>Names - Results</h1>';
-
-echo '<h2>All Names</h2>';
+echo '<section style="color: #fff; display: flex; gap: 4rem; justify-content: center">';
+echo '<section>';
+echo '<h2 style="text-decoration: underline">All Names</h2>';
 echo "<p>There are " . sizeof($fullNames) . " total names</p>";
 echo '<ul style="list-style-type:none">';    
     foreach($fullNames as $fullName) {
         echo "<li>$fullName</li>";
     }
 echo "</ul>";
+echo '</section>';
 
-echo '<h2>All Valid Names</h2>';
+echo '<section>';
+echo '<h2 style="text-decoration: underline">All Valid Names</h2>';
 echo "<p>There are " . sizeof($validFullNames) . " valid names</p>";
 echo '<ul style="list-style-type:none">';    
     foreach($validFullNames as $validFullName) {
         echo "<li>$validFullName</li>";
     }
 echo "</ul>";
+echo '</section>';
+echo '</section>';
 
-echo '<h2>Unique Full Names</h2>';
+echo '<section style="color: #fff; display: flex; gap: 4rem; justify-content: center">';
+echo '<section>';
+echo '<h2 style="text-decoration: underline">Unique Full Names</h2>';
 echo ("<p>There are " . sizeof($uniqueValidNames) . " Unique full names</p>");
 echo '<ul style="list-style-type:none">';    
     foreach($uniqueValidNames as $uniqueValidNames) {
         echo "<li>$uniqueValidNames</li>";
     }
 echo "</ul>";
+echo '</section>';
 
-echo '<h2>Unique Last Names</h2>';
+echo '<section>';
+echo '<h2 style="text-decoration: underline">Unique Last Names</h2>';
 echo ("<p>There are " . sizeof($uniqueValidLastNames) . " Unique last names</p>");
 echo '<ul style="list-style-type:none">';    
     foreach($uniqueValidLastNames as $uniqueValidLastNames) {
         echo "<li>$uniqueValidLastNames</li>";
     }
 echo "</ul>";
+echo '</section>';
 
-echo '<h2>Unique First Names</h2>';
+echo '<section>';
+echo '<h2 style="text-decoration: underline">Unique First Names</h2>';
 echo ("<p>There are " . sizeof($uniqueValidFirstNames) . " Unique first names</p>");
 echo '<ul style="list-style-type:none">';    
     foreach($uniqueValidFirstNames as $uniqueValidFirstNames) {
         echo "<li>$uniqueValidFirstNames</li>";
     }
 echo "</ul>";
+echo '</section>';
+echo '</section>';
 
-echo '<h2>Most Common Last Names</h2>';
+echo '<section style="color: #fff; display: flex; gap: 4rem; justify-content: center">';
+echo '<section>';
+echo '<h2 style="text-decoration: underline">Most Common Last Names</h2>';
 echo ("<p>The most common last name in this file is $mostCommonLastName.</p>");
 echo ("<p>It appears $lastNameOccurrences time(s).</p>");
+echo '</section>';
 
-echo '<h2>Most Common First Names</h2>';
+echo '<section>';
+echo '<h2 style="text-decoration: underline">Most Common First Names</h2>';
 echo ("<p>The most common first name in this file is $mostCommonFirstName.</p>");
 echo ("<p>It appears $firstNameOccurrences time(s).</p>");
+echo '</section>';
+echo '</section>';
+echo '</section>';
+echo '</body>';
 ?>
